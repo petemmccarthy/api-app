37fe9ee8c91190079eddee4530bb1fdbe0f7ac5c
Pete M McCarthy <pete.m.mccarthy@gmail.com>
2016-06-02 20:57:29 +0100

    showCust and Premises actions added

--- app/home/route.js
+++ app/home/route.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    showCustomers () {
+      this.transitionTo('customers');
+    },
+    showPremises () {
+      this.transitionTo('customers');
+    }
+  }
 });
